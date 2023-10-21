11808e6ca01c582d889ea9693449214310dfae2e
Giselle Torres <giselita6313@gmail.com>
2023-10-21 18:48:49 -0500

    arreglar variables de entorno

--- src/conexion/index.js
+++ src/conexion/index.js
@@ -1,18 +1,25 @@
+require('dotenv').config()
 var mysql = require('mysql');
-var settings = require('./config.json');
 var connection;
 
 function connectDatabase() 
 {
+  console.log(process.env.DB_NAME)
   if(!connection) 
   {
-    connection = mysql.createConnection(settings);
+    connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: 3306,
+   });
 
     connection.connect(function(err)
     {
       if(!err) 
       {
-        console.log('Base de Datos Conectada' + settings.database );
+        console.log('Base de Datos Conectada' + process.env.DB_NAME);
       } 
       else 
       {
